52c59fc1488108db96fb11061ba49ee2bc06166c
shubham423 <jethurishubham@gmail.com>
2021-11-03 21:49:48 +0530

    districts spinner working

--- app/src/main/java/com/example/vaccinator/ui/vaccination/DistrictFragment.kt
+++ app/src/main/java/com/example/vaccinator/ui/vaccination/DistrictFragment.kt
@@ -2,16 +2,15 @@ package com.example.vaccinator.ui.vaccination
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.core.view.get
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.example.utils.showDLog
 import com.example.vaccinator.R
+import com.example.vaccinator.data.models.DistrictsResponse.District
 import com.example.vaccinator.data.models.StatesResponse.*
 import com.example.vaccinator.databinding.FragmentDistrictBinding
 import com.example.vaccinator.viewmodels.VaccinationViewModel
@@ -25,6 +24,7 @@ class DistrictFragment : Fragment() {
 
     private var states: ArrayList<String> = ArrayList()
     private var statesObjects: ArrayList<State> = ArrayList()
+    private var districtsObjects: ArrayList<District> = ArrayList()
     private var districts: ArrayList<String> = ArrayList()
 
 
@@ -41,64 +41,35 @@ class DistrictFragment : Fragment() {
 
         viewModel.getStates()
         initObservers()
-        Log.d("District","initobsever below $states")
+        Log.d("District", "initobsever below $states")
 
-        val adapterStates = ArrayAdapter(
-            requireActivity(),
-            R.layout.support_simple_spinner_dropdown_item,
-            states
-        )
+        val stateAdapter =
+            ArrayAdapter(requireContext(), R.layout.dropdown_item, states)
+        binding.statesSpinnerTv.setAdapter(stateAdapter)
+        binding.statesSpinnerTv.setOnItemClickListener{_, _, position, _ ->
+            Toast.makeText(requireContext(), states[position], Toast.LENGTH_SHORT).show()
+            binding.districtSpinnerTv.isEnabled = true
+            districts.clear()
+            districtsObjects.clear()
+            handleDistricts(position)
+            binding.statesSpinnerTv.error = null
 
-        binding.statesSpinner.adapter = adapterStates
-        adapterStates.notifyDataSetChanged()
-
-
-        binding.statesSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                binding.tvState.text =states[position]
-                Log.d("District","onitem $statesObjects")
-                for (i in statesObjects){
-                    Log.d("District","${i.stateName} and ${parent?.getItemAtPosition(position).toString()}")
-                    if (i.stateName==parent?.getItemAtPosition(position).toString()){
-                        viewModel.setSelectedState(i.stateId.toString())
-                        return
-                    }
-                }
-                Log.d("state id","${viewModel.selectedStateId}")
-                viewModel.selectedStateId.value?.let { viewModel.getDistricts(it) }
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                //no ops
-            }
         }
 
-        val adapterDistricts = ArrayAdapter<String>(
-            requireContext(),
-            R.layout.support_simple_spinner_dropdown_item,
-            districts
-        )
+    }
 
-        binding.districtSpinner.onItemSelectedListener=object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                binding.tvDistricts.text= parent?.get(position).toString()
-            }
+    private fun handleDistricts(position: Int) {
+        viewModel.getDistricts(position.toString())
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                //no ops
-            }
+        val districtsAdapter =
+            ArrayAdapter(requireContext(), R.layout.dropdown_item, districts)
+        binding.districtSpinnerTv.apply {
+            setAdapter(districtsAdapter)
         }
 
+        binding.districtSpinnerTv.setOnItemClickListener { parent, view, position, id ->
+            binding.districtSpinnerTv.error = null
+        }
     }
 
     private fun initObservers() {
@@ -109,17 +80,15 @@ class DistrictFragment : Fragment() {
                     states.add(i.stateName)
                 }
             }
-
         }
         viewModel.districtsList.observe(viewLifecycleOwner) {
             districts.clear()
             if (it.isNotEmpty()) {
                 for (i in it) {
                     districts.add(i.districtName)
+                    districtsObjects.add(i)
                 }
             }
         }
     }
-
-
 }
